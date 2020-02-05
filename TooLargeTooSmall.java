d9acdffd590d8acf5dbaad45edef07fefb194588
Leila Hsiao <lsiao@zipcodes-MacBook-Pro-3.local>
2020-02-05 09:54:29 -0500

    finished

--- TooLargeTooSmall.java
+++ TooLargeTooSmall.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class TooLargeTooSmall here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Leila Hsiao
+ * @version 2/5/2020
  */
 public class TooLargeTooSmall {
     private Integer chosenNum;
@@ -17,8 +17,18 @@ public class TooLargeTooSmall {
     }
     
     public Integer guess(Integer g) {
+        Integer res = 0;
         
-        return chosenNum;
-
+        //if the guess number is bigger then the given seed, return1
+        //if the guess number is smaller then the given seed, return-1
+        //if the guess number is equal to the given seed, return0
+        if( chosenNum < g ){
+            res = 1;
+        }else if( chosenNum > g ){            
+            res = -1;
+        }else{
+            res = 0;
+        }        
+        return res;
     }
 }
